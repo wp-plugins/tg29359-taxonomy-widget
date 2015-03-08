dc42b552664082ab3e51a87922bc63c50ae4af3d
tg29359 <tg29359@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-08 07:57:07 +0000

    draft for version 0.0.4
    git-svn-id: https://plugins.svn.wordpress.org/tg29359-taxonomy-widget/trunk@1107954 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -13,7 +13,31 @@ Displays a list or dropdown of taxonomies in a sidebar widget.
 
 This plugin displays a list or dropdown of taxonomies in a sidebar widget.
 
+== Installation ==
+
+1. Download `tg29359-taxonomy-widget.zip`, upload it to your server, expand it as `/wp-content/plugins/tg29359-taxonomy-widget/`.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Add `tg29359 taxonomy widget` to your sidebar through the 'Appearance/Widget' menu in WordPress.
+
+== Frequently Asked Questions ==
+
+= tg29359, are you Japanese? =
+
+Yes, I am.
+
+== Screenshots ==
+
+Not coming soon.
+
 == Changelog ==
+
+= 0.0.4 =
+* Changed 'Custom Select Taxonomy:' to 'Replace "Select Taxonomy" of dropdown to:'
+* Changed 'Hide Empty' to 'Hide empty taxonomy'
+* Added 'Hide empty dropdown'
+* Added 'Replace "No taxonomies" of list to:'
+* Added 'Include post counts of child taxonomies'
+* Added 'Order by' and 'Order' (but they does not work yet)
 
 = 0.0.3 =
 * Changed the character for displaying hierarchical indention from non-breaking space to hyphen. Because non-breaking space is ignored on iOS 8, and dropdown is displayed flat even though "Show hierarchy" is checked.
@@ -24,3 +48,16 @@ This plugin displays a list or dropdown of taxonomies in a sidebar widget.
 = 0.0.1 =
 * The first version of this plugin.
 
+== Upgrade Notice ==
+
+= 0.0.4 =
+User of the previous version do not need to upgrade to this version.
+But if you want to use new feature that is described in Changelog section, upgrade to this version.
+
+= 0.0.3 =
+User of the previous version do not need to upgrade to this version.
+But if you want to display hierarchical dropdown on iOS, upgrade to this version.
+
+= 0.0.2 =
+User of the previous version do not need to upgrade to this version.
+The plugin functionality has not been changed in this version.
--- tg29359-taxonomy-widget.php
+++ tg29359-taxonomy-widget.php
@@ -6,7 +6,7 @@
 Plugin Name: tg29359 taxonomy widget
 Plugin URI: http://tg29359.rdy.jp/app/tg29359-taxonomy-widget/
 Description: Displays a list or dropdown of taxonomies in a sidebar widget.
-Version: 0.0.3
+Version: 0.0.4
 Author: tg29359
 Author URI: http://tg29359.rdy.jp/
 DomainPath: /languages
@@ -75,11 +75,23 @@ class tg29359_Widget_Taxonomies extends WP_Widget
         $title = apply_filters('widget_title', empty($instance['title']) ? __('Taxonomies', 'tg29359-taxonomy-widget') : $instance['title'], $instance, 'tg29359_taxonomies');
 
         $c = !empty($instance['count']) ? '1' : '0';
+        $pad_counts = !empty($instance['pad_counts']) ? '1' : '0';
         $h = !empty($instance['hierarchical']) ? '1' : '0';
         $hide_empty = !empty($instance['hide_empty']) ? '1' : '0';
+        $hide_if_empty = !empty($instance['hide_if_empty']) ? '1' : '0';
         $d = !empty($instance['dropdown']) ? '1' : '0';
         if (!empty($instance['taxonomy'])) {
             $this->taxonomy = $instance['taxonomy'];
+        }
+        if (!empty($instance['orderby'])) {
+            $orderby = $instance['orderby'];
+        } else {
+            $orderby = '';
+        }
+        if (!empty($instance['order'])) {
+            $order = $instance['order'];
+        } else {
+            $order = '';
         }
 
         echo $args['before_widget'];
@@ -87,22 +99,20 @@ class tg29359_Widget_Taxonomies extends WP_Widget
             echo $args['before_title'] . $title . $args['after_title'];
         }
 
-        $cat_args = array('orderby' => 'name', 'show_count' => $c, 'hierarchical' => $h, 'hide_empty' => $hide_empty);
-
+        $cat_args = array('taxonomy' => $this->taxonomy, 'show_count' => $c, 'pad_counts' => $pad_counts, 'hierarchical' => $h, 'hide_empty' => $hide_empty, 'orderby' => $orderby, 'order' => $order);
         if ($d) {
             if (!empty($instance['select_xxx_for_dropdown'])) {
                 $cat_args['show_option_none'] = $instance['select_xxx_for_dropdown'];
             } else {
                 $cat_args['show_option_none'] = __('Select Taxonomy', 'tg29359-taxonomy-widget');
             }
-            $cat_args['taxonomy'] = $this->taxonomy;
             $cat_args['id'  ] = 'tg29359_taxonomies_tax';
             $cat_args['name'] = 'tg29359_taxonomies_tax';
             $cat_args['walker'] = $this->walker;
+	        $cat_args['hide_if_empty'] = $hide_if_empty;
 
             wp_dropdown_categories(apply_filters('widget_categories_dropdown_args', $cat_args));
 ?>
-
 <script type='text/javascript'>
 /* <![CDATA[ */
     var tg29359_taxonomies_dropdown = document.getElementById('tg29359_taxonomies_tax');
@@ -115,18 +125,21 @@ class tg29359_Widget_Taxonomies extends WP_Widget
     tg29359_taxonomies_dropdown.onchange = tg29359_taxonomies_onTaxonomyChange;
 /* ]]> */
 </script>
-
 <?php
         } else {
 ?>
-        <ul>
+            <ul>
 <?php
-        $cat_args['title_li'] = '';
-        $cat_args['taxonomy'] = $this->taxonomy;
+            $cat_args['title_li'] = '';
+            if (!empty($instance['no_taxonomies_for_list'])) {
+                $cat_args['show_option_none'] = $instance['no_taxonomies_for_list'];
+            } else {
+                $cat_args['show_option_none'] = __('No taxonomies', 'tg29359-taxonomy-widget');
+            }
 
-        wp_list_categories(apply_filters('widget_categories_args', $cat_args));
+            wp_list_categories(apply_filters('widget_categories_args', $cat_args));
 ?>
-        </ul>
+            </ul>
 <?php
         }
         echo $args['after_widget'];
@@ -137,14 +150,28 @@ class tg29359_Widget_Taxonomies extends WP_Widget
         $instance = $old_instance;
         $instance['title'] = strip_tags($new_instance['title']);
         $instance['select_xxx_for_dropdown'] = strip_tags($new_instance['select_xxx_for_dropdown']);
+        $instance['no_taxonomies_for_list'] = strip_tags($new_instance['no_taxonomies_for_list']);
         $instance['count'] = !empty($new_instance['count']) ? 1 : 0;
+        $instance['pad_counts'] = !empty($new_instance['pad_counts']) ? 1 : 0;
         $instance['hierarchical'] = !empty($new_instance['hierarchical']) ? 1 : 0;
         $instance['hide_empty'] = !empty($new_instance['hide_empty']) ? 1 : 0;
+        $instance['hide_if_empty'] = !empty($new_instance['hide_if_empty']) ? 1 : 0;
         $instance['dropdown'] = !empty($new_instance['dropdown']) ? 1 : 0;
+
         $taxonomies = $this->get_taxonomies();
         $instance['taxonomy'] = 'category';
         if (in_array($new_instance['taxonomy'], $taxonomies)) {
             $instance['taxonomy'] = $new_instance['taxonomy'];
+        }
+
+        $orderby = array('name', 'ID', 'slug', 'count', 'term_group', 'menu_order');
+        if (in_array($new_instance['orderby'], $orderby)) {
+            $instance['orderby'] = $new_instance['orderby'];
+        }
+        
+        $order = array('ASC', 'DESC');
+        if (in_array($new_instance['order'], $order)) {
+            $instance['order'] = $new_instance['order'];
         }
         return $instance;
     }
@@ -156,9 +183,13 @@ class tg29359_Widget_Taxonomies extends WP_Widget
         $title = esc_attr($instance['title']);
         $instance = wp_parse_args((array)$instance, array( 'select_xxx_for_dropdown' => ''));
         $select_xxx_for_dropdown = esc_attr($instance['select_xxx_for_dropdown']);
+        $instance = wp_parse_args((array)$instance, array( 'no_taxonomies_for_list' => ''));
+        $no_taxonomies_for_list = esc_attr($instance['no_taxonomies_for_list']);
         $count = isset($instance['count']) ? (bool)$instance['count'] : false;
+        $pad_counts = isset($instance['pad_counts']) ? (bool)$instance['pad_counts'] : false;
         $hierarchical = isset($instance['hierarchical']) ? (bool)$instance['hierarchical'] : false;
         $hide_empty = isset($instance['hide_empty']) ? (bool)$instance['hide_empty'] : false;
+        $hide_if_empty = isset($instance['hide_if_empty']) ? (bool)$instance['hide_if_empty'] : false;
         $dropdown = isset($instance['dropdown']) ? (bool) $instance['dropdown'] : false;
         $taxonomy = 'category';
         if (!empty( $instance['taxonomy'])) {
@@ -169,21 +200,31 @@ class tg29359_Widget_Taxonomies extends WP_Widget
         <p>
             <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e( 'Title:' ); ?></label>
             <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
-            <label for="<?php echo $this->get_field_id('select_xxx_for_dropdown'); ?>"><?php _e( 'Custom Select Taxonomy:', 'tg29359-taxonomy-widget'); ?></label>
+            <br />
+            <label for="<?php echo $this->get_field_id('select_xxx_for_dropdown'); ?>"><?php _e( 'Replace "Select Taxonomy" of dropdown to:', 'tg29359-taxonomy-widget'); ?></label>
             <input class="widefat" id="<?php echo $this->get_field_id('select_xxx_for_dropdown'); ?>" name="<?php echo $this->get_field_name('select_xxx_for_dropdown'); ?>" type="text" value="<?php echo $select_xxx_for_dropdown; ?>" />
+            <br />
+            <label for="<?php echo $this->get_field_id('no_taxonomies_for_list'); ?>"><?php _e( 'Replace "No taxonomies" of list to:', 'tg29359-taxonomy-widget'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('no_taxonomies_for_list'); ?>" name="<?php echo $this->get_field_name('no_taxonomies_for_list'); ?>" type="text" value="<?php echo $no_taxonomies_for_list; ?>" />
         </p>
         <p>
             <input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id('dropdown'); ?>" name="<?php echo $this->get_field_name('dropdown'); ?>"<?php checked( $dropdown ); ?> />
-            <label for="<?php echo $this->get_field_id('dropdown'); ?>"><?php _e( 'Display as dropdown' ); ?></label><br />
-
-            <input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id('count'); ?>" name="<?php echo $this->get_field_name('count'); ?>"<?php checked( $count ); ?> />
-            <label for="<?php echo $this->get_field_id('count'); ?>"><?php _e( 'Show post counts' ); ?></label><br />
-
+            <label for="<?php echo $this->get_field_id('dropdown'); ?>"><?php _e( 'Display as dropdown' ); ?></label>
+            <br />
             <input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id('hierarchical'); ?>" name="<?php echo $this->get_field_name('hierarchical'); ?>"<?php checked( $hierarchical ); ?> />
-            <label for="<?php echo $this->get_field_id('hierarchical'); ?>"><?php _e( 'Show hierarchy' ); ?></label><br />
-
+            <label for="<?php echo $this->get_field_id('hierarchical'); ?>"><?php _e( 'Show hierarchy' ); ?></label>
+            <br />
+            <input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id('count'); ?>" name="<?php echo $this->get_field_name('count'); ?>"<?php checked( $count ); ?> />
+            <label for="<?php echo $this->get_field_id('count'); ?>"><?php _e( 'Show post counts' ); ?></label>
+            <br />
+            <input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id('pad_counts'); ?>" name="<?php echo $this->get_field_name('pad_counts'); ?>"<?php checked( $pad_counts ); ?> />
+            <label for="<?php echo $this->get_field_id('pad_counts'); ?>"><?php _e('Include post counts of child taxonomies', 'tg29359-taxonomy-widget'); ?></label>
+            <br />
             <input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id('hide_empty'); ?>" name="<?php echo $this->get_field_name('hide_empty'); ?>"<?php checked( $hide_empty ); ?> />
-            <label for="<?php echo $this->get_field_id('hide_empty'); ?>"><?php _e( 'Hide Empty', 'tg29359-taxonomy-widget'); ?></label>
+            <label for="<?php echo $this->get_field_id('hide_empty'); ?>"><?php _e( 'Hide empty taxonomy', 'tg29359-taxonomy-widget'); ?></label>
+            <br />
+            <input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id('hide_if_empty'); ?>" name="<?php echo $this->get_field_name('hide_if_empty'); ?>"<?php checked( $hide_if_empty ); ?> />
+            <label for="<?php echo $this->get_field_id('hide_if_empty'); ?>"><?php _e( 'Hide empty dropdown', 'tg29359-taxonomy-widget'); ?></label>
         </p>
         <p>
             <label for="<?php echo $this->get_field_id( 'taxonomy' ); ?>"><?php _e( 'Taxonomy:' ); ?></label><br />
@@ -192,6 +233,26 @@ class tg29359_Widget_Taxonomies extends WP_Widget
                 <option value="<?php echo esc_attr( $value ); ?>"<?php selected( $taxonomy, $value ); ?>><?php echo esc_attr( $value ); ?></option>
                 <?php endforeach; ?>
             </select>
+        </p>
+        <p>
+            <label for="<?php echo $this->get_field_name('orderby'); ?>"><?php _e('Sort By:', 'tg29359-taxonomy-widget'); ?></label>
+            <select name="<?php echo $this->get_field_name('orderby'); ?>" class="widefat">
+                <option value="name"       <?php selected('name'      , $instance['orderby']); ?>><?php _e('Name'                                 ); ?></option>
+                <option value="ID"         <?php selected('ID'        , $instance['orderby']); ?>><?php _e('ID'        , 'tg29359-taxonomy-widget'); ?></option>
+                <option value="slug"       <?php selected('slug'      , $instance['orderby']); ?>><?php _e('Slug'                                 ); ?></option>
+                <option value="count"      <?php selected('count'     , $instance['orderby']); ?>><?php _e('Count'     , 'tg29359-taxonomy-widget'); ?></option>
+                <option value="term_group" <?php selected('term_group', $instance['orderby']); ?>><?php _e('Term group', 'tg29359-taxonomy-widget'); ?></option>
+                <option value="menu_order" <?php selected('menu_order', $instance['orderby']); ?>><?php _e('Menu order'); ?></option>
+            </select>
+            <span><?php _e('*Sorry, this feature does not work yet, at least with the plugin "Category Order and Taxonomy Terms Order".', 'tg29359-taxonomy-widget'); ?></span>
+        </p>
+        <p>
+            <label for="<?php echo $this->get_field_name('order'); ?>"><?php _e('Sort Order:'); ?></label>
+            <select name="<?php echo $this->get_field_name( 'order' ); ?>" class="widefat">
+                <option value="ASC"  <?php selected('ASC' , $instance['order']); ?>><?php _e('Ascending' ); ?></option>
+                <option value="DESC" <?php selected('DESC', $instance['order']); ?>><?php _e('Descending'); ?></option>
+            </select>
+            <span><?php _e('*Sorry, this feature does not work yet, at least with the plugin "Category Order and Taxonomy Terms Order".', 'tg29359-taxonomy-widget'); ?></span>
         </p>
 <?php
     }
